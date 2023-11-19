2ae59f34baaf57874b2bd5cac72e1b6603e7e84d
MANUELBSFILHO <mbsfilho.engenharia@gmail.com>
2023-11-19 18:19:07 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let totalVotes = 0;
 let option1Votes = 0;
 let option2Votes = 0;
+let option3Votes = 0;
+let option4Votes = 0;
 
 function vote(option) {
     totalVotes++;
@@ -12,6 +14,12 @@ function vote(option) {
     } else if (option === 2) {
         option2Votes++;
         updateOptionVotes(2, option2Votes);
+    } else if (option === 3) {
+        option3Votes++;
+        updateOptionVotes(3, option3Votes);
+    } else if (option === 4) {
+        option4Votes++;
+        updateOptionVotes(4, option4Votes);
     }
     // Adicione mais lógica para outras opções conforme necessário.
 }
@@ -23,7 +31,6 @@ function updateTotalVotes() {
 function updateOptionVotes(option, votes) {
     document.getElementById("option" + option + "Votes").innerText = votes;
     
-    // Calcular a porcentagem
     const percentage = ((votes / totalVotes) * 100).toFixed(2);
     document.getElementById("option" + option + "Percentage").innerText = percentage;
 }
